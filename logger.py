f9736a0147cf2fb0dc5df6f1b0460c163113da78
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2024-03-03 15:05:14 -0800

    handle graceful quitting

--- logger.py
+++ logger.py
@@ -2,11 +2,33 @@
 
 
 import logging
+import os
 
 
 class Logger:
     def __init__(self):
-        logging.basicConfig()
-        logging.root.setLevel(logging.INFO)
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger('chat-app')
+        # Create a logger
+        self.logger = logging.getLogger('my_logger')
+        self.logger.setLevel(logging.DEBUG)
+
+        # Create a formatter
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        # Create a handler for writing log messages to the console (stdout)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)  # Set the desired log level for the console output
+        console_handler.setFormatter(formatter)
+
+        try:
+            os.makedirs("/tmp/chatApp/logs")
+        except FileExistsError:
+            pass
+        # Create a handler for writing log messages to a file
+        current_pid_file = "/tmp/chatApp/logs/" + str(os.getpid()) + ".log"
+        file_handler = logging.FileHandler(current_pid_file)
+        file_handler.setLevel(logging.DEBUG)  # Set the desired log level for the file output
+        file_handler.setFormatter(formatter)
+
+        # Add both handlers to the logger
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
--- terminal.py
+++ terminal.py
@@ -2,18 +2,19 @@ import curses
 import logger
 import socket
 import threading
-import sys
+
 
 HOST = '0.0.0.0'
 PORT = 3874
+
 
 class TerminalApp:
     def __init__(self, stdscr):
         self.stdscr = stdscr
         self.logger = logger.Logger().logger
         self.close = False
-        curses.curs_set(1)  # Hide the cursor
 
+        curses.curs_set(1)  # Hide the cursor
         curses.use_default_colors()
         # Get the background color of the terminal
         self.bg_color = curses.color_pair(0)
@@ -27,29 +28,6 @@ class TerminalApp:
         self.output_win = curses.newwin(curses.LINES-1, curses.COLS, 0, 0)
 
         self.create_client(HOST, PORT)
-
-    def run(self):
-        try:
-            while True:
-                # Get user input
-                user_input = self.get_input()
-                if user_input in ["q", "quit", "exit"]:
-                    break
-
-                # Display the entered command in the output area
-                self.output_win.addstr(f">>> {user_input}\n", self.bg_color)
-                self.output_win.refresh()
-
-                # Execute the command
-                result = self.run_command(user_input)
-
-                # Display the result in the output area
-                self.output_win.addstr(f"{result}\n", self.bg_color)
-                self.output_win.refresh()
-
-        except KeyboardInterrupt:
-            self.logger.info("Shutting down client.")
-            
 
     def get_input(self):
         # Allow the user to enter input
@@ -71,44 +49,47 @@ class TerminalApp:
 
     def send_message(self, sock):
         # write code for sending message here.
-        try:
-            while True:
-                input_string = self.get_input()
-                self.input_win.refresh()
-                if input_string not in ["exit", "quit", "q"]:
-                    sock.send(input_string.encode())
-                    self.output_win.addstr(str(sock.getpeername()[1]) + "\n" + input_string + "\n")
-                    self.output_win.refresh()
-                    self.input_win.refresh()
-                else:
-                    sock.send(input_string.encode())
-                    break
+        while True:
+            input_string = self.get_input()
+            self.output_win.refresh()
+            self.input_win.refresh()
+            if input_string in ["exit", "quit", "q"]:
+                break
+            else:
+                sock.send(input_string.encode())
+                self.output_win.addstr("\n" + input_string + "\n")
                 self.output_win.refresh()
                 self.input_win.refresh()
-
-        except KeyboardInterrupt:
-            self.logger.info("Shutting down client")
-            sock.send("q".encode())
-            data = sock.recv(1024)
-            self.logger.info('received from server %s' %repr(data.decode('utf-8')))
-        except BrokenPipeError:
-            self.logger.info("Connection to server broken, shutting down client")
+        sock.send("q".encode())
         self.close = True
 
     def create_client(self, hostname, port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((hostname, port))
-            send_thread = threading.Thread(target=self.send_message, args=((sock,)), daemon=True)
-            send_thread.start()
-            while True:
+            try:
+                sock.connect((hostname, port))
+                send_thread = threading.Thread(target=self.send_message, args=((sock,)), daemon=True)
+                send_thread.start()
+                while True:
+                    data = sock.recv(1024)
+                    if self.close:
+                        self.logger.info("Closing client, server responded with: %s"%data.decode('utf-8'))
+                        break
+                    if data:
+                        self.output_win.addstr("\n    %s\n"%data.decode())
+                        self.output_win.refresh()
+                        self.input_win.refresh()
+
+            except KeyboardInterrupt:
+                self.logger.info("Shutting down client")
+                sock.send("q".encode())
                 data = sock.recv(1024)
-                if data:
-                    self.output_win.addstr("\n    %s\n"%data.decode())
-                    self.output_win.refresh()
-                    self.input_win.refresh()
-                if self.close:
-                    break
+                self.logger.info('received from server: %s' %data.decode('utf-8'))
+            except BrokenPipeError:
+                self.logger.info("Connection to server broken, shutting down client")
+
+            self.logger.info("Shutting down client finally")
             sock.close()
+
 
 if __name__ == "__main__":
     curses.wrapper(TerminalApp)
